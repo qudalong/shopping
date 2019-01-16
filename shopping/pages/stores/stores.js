5eb6d050f5135cf03e84edf855a6959d87e43a38
qudalong <862792055@qq.com>
2019-01-16 11:15:24 +0800

    模块整合

--- shopping/pages/stores/stores.js
+++ shopping/pages/stores/stores.js
@@ -5,62 +5,98 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    list: [{
+      img: '/image/img/banner01.jpg',
+      raking: [1, 1, 1, 1],
+      name: '大卫城',
+      tel: '18768874896',
+      distance: 541
+    }, {
+      img: '/image/img/banner02.jpg',
+      raking: [1, 1, 1],
+      name: '国贸360',
+      tel: '15159844557',
+      distance: 954
+    }, {
+      img: '/image/img/banner03.jpg',
+      raking: [1, 1, 1, 1, 1],
+      name: '二七广场',
+      tel: '19726546544',
+      distance: 200
+    }],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
+  shopDetails() {
+    console.log(9)
+  },
+
+  getAdress() {
+    wx.getLocation({
+      type: 'gcj02',
+      success(res) {
+        const latitude = res.latitude
+        const longitude = res.longitude
+        wx.openLocation({
+          latitude,
+          longitude,
+          scale: 18
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
