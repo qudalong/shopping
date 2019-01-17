b0a9e0a3e8ca9d5e0f7b3808005e802d67260c1e
qudalong <862792055@qq.com>
2019-01-17 15:08:36 +0800

    结算整合

--- shopping/pages/my/my.js
+++ shopping/pages/my/my.js
@@ -11,11 +11,32 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
-  tel: function () {
+  getPhoneNumber: function(e) {
+    console.log(e.detail.errMsg)
+    console.log(e.detail.iv)
+    console.log(e.detail.encryptedData)
+    if (e.detail.errMsg == 'getPhoneNumber:fail user deny') {
+      wx.showModal({
+        title: '提示',
+        showCancel: false,
+        content: '未授权',
+        success: function(res) {}
+      })
+    } else {
+      wx.showModal({
+        title: '提示',
+        showCancel: false,
+        content: '同意授权',
+        success: function(res) {}
+      })
+    }
+  },
+
+  tel: function() {
     wx.makePhoneCall({
       phoneNumber: '18768871896',
     })
@@ -25,6 +46,18 @@ Page({
     const activeIndex = e.currentTarget.dataset.index;
     wx.navigateTo({
       url: `/pages/orderList/orderList?activeIndex=${activeIndex}`
+    })
+  },
+
+  toAboutUs() {
+    wx.navigateTo({
+      url: `/pages/aboutUs/aboutUs`
+    })
+  },
+
+  toAddress() {
+    wx.navigateTo({
+      url: `/pages/addAddress/addAddress`
     })
   },
 
@@ -43,49 +76,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
